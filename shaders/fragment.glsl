0a459dd81e65953fd43a710b3737411d2027e3e3
Emerson <47287033+emersondsnunes@users.noreply.github.com>
2026-05-17 19:28:08 -0300

    Add files via upload

---
+++ shaders/fragment.glsl
@@ -0,0 +1,22 @@
+#version 330 core
+
+out vec4 FragColor;
+
+in vec2 TexCoord;
+
+uniform sampler2D cardTexture;
+
+uniform bool useTexture;
+uniform vec4 solidColor;
+
+void main()
+{
+    if (useTexture)
+    {
+        FragColor = texture(cardTexture, TexCoord);
+    }
+    else
+    {
+        FragColor = solidColor;
+    }
+}
